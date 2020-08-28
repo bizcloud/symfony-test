23e54f2aab34738bdddd29752d7d5e9a324e3aab
Alexandr Fefelov <slstudio@yandex.ru>
2020-08-28 17:37:15 +0000

    Fixtures for Maker Entity added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @throws \Nelmio\Alice\Throwable\LoadingThrowable
+     *
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $objectSet = (new NativeLoader())->loadFile(__DIR__.'/../../fixtures/data.yml');
+        foreach ($objectSet->getObjects() as $object) {
+            $manager->persist($object);
+        }
 
         $manager->flush();
     }
