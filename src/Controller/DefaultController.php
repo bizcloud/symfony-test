9ce1cd96105475539100449167d20c9a7da6d410
Alexandr Fefelov <slstudio@yandex.ru>
2020-08-29 23:18:22 +0000

    Default controller and test

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @Template()
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return [];
+    }
+}
---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    /** @test */
+    public function index_requestNoParameters_selectorContainsText(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Каталог автомобилей 2');
+    }
+}
